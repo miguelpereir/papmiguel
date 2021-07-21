a1b28cb68bcf1ba8dfd12b3abdf146f786c2f17e
migue <miguel_pereira_21@hotmail.com>
2021-07-22 00:10:10 +0100

    update

--- admin/eliminaJogador.php
+++ admin/eliminaJogador.php
@@ -2,6 +2,8 @@
 include_once ("../includes/body.inc.php");
 $id=intval($_GET['id']);
 echo $sql= "delete from jogadores where jogadorId=".$id;
+echo $sql2= "delete from equipajogadores where equipaJogadorJogadorId=".$id;
+mysqli_query($con,$sql2);
 mysqli_query($con,$sql);
 header("location:jogadores.php");
 ?>
